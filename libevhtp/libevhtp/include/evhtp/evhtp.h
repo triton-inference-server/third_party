3174fc22511299cea03fe223ad56e4c7626b988b
9cvele3 <9cvele3@gmail.com>
2021-09-20 20:54:29 +0200

    Changes in libevhtp for http support on windows (#9)
    * patches in libevhtp for http support on windows
    
    * libevhtp polishing
    
    * remove indent
    
    * SIZEOF_SSIZET in cmake
    
    * fix linux build again
    
    * added patch files

--- libevhtp/libevhtp/include/evhtp/evhtp.h
+++ libevhtp/libevhtp/include/evhtp/evhtp.h
@@ -18,7 +18,14 @@
 #include <onigposix.h>
 #endif
 
+// either system or compat header
+#ifdef HAVE_SYS_QUEUE
 #include <sys/queue.h>
+#else
+#include "sys/queue.h"
+#endif
+
+
 #include <event2/event.h>
 #include <event2/listener.h>
 #include <event2/buffer.h>
@@ -614,7 +621,9 @@ EVHTP_EXPORT int evhtp_ssl_init(evhtp_t * htp, evhtp_ssl_cfg_t * ssl_cfg);
  *
  * @param htp
  */
-EVHTP_EXPORT void evhtp_disable_100_continue(evhtp_t * htp)
+EVHTP_EXPORT
+WINDEPRECATED("evhtp_disable_100 will soon be deprecated, use htp->flags instead")
+void evhtp_disable_100_continue(evhtp_t * htp)
 DEPRECATED("evhtp_disable_100 will soon be deprecated, use htp->flags instead");
 
 /**
@@ -865,7 +874,9 @@ EVHTP_EXPORT int evhtp_bind_sockaddr(evhtp_t * htp, struct sockaddr *,
  *
  * @return
  */
-EVHTP_EXPORT int evhtp_use_threads(evhtp_t *, evhtp_thread_init_cb, int nthreads, void *)
+EVHTP_EXPORT
+WINDEPRECATED("will take on the syntax of evhtp_use_threads_wexit")
+int evhtp_use_threads(evhtp_t *, evhtp_thread_init_cb, int nthreads, void *)
 DEPRECATED("will take on the syntax of evhtp_use_threads_wexit");
 
 /**
--- libevhtp/libevhtp/include/evhtp/thread.h
+++ libevhtp/libevhtp/include/evhtp/thread.h
@@ -35,8 +35,10 @@ typedef void (* evthr_cb)(evthr_t * thr, void * cmd_arg, void * shared);
 typedef void (* evthr_init_cb)(evthr_t * thr, void * shared);
 typedef void (* evthr_exit_cb)(evthr_t * thr, void * shared);
 
-EVHTP_EXPORT evthr_t * evthr_new(evthr_init_cb, void *)
-    DEPRECATED("will take on the syntax of evthr_wexit_new");
+EVHTP_EXPORT
+WINDEPRECATED("will take on the syntax of evthr_wexit_new")
+evthr_t * evthr_new(evthr_init_cb, void *)
+DEPRECATED("will take on the syntax of evthr_wexit_new");
 
 EVHTP_EXPORT evbase_t     * evthr_get_base(evthr_t * thr);
 EVHTP_EXPORT void           evthr_set_aux(evthr_t * thr, void * aux);
@@ -49,8 +51,10 @@ EVHTP_EXPORT void           evthr_free(evthr_t * evthr);
 /* Detail: https://github.com/criticalstack/libevhtp/issues/146#issuecomment-696339470 */
 EVHTP_EXPORT void           evthr_set_busy(evthr_t * thr, int busy);
 
-EVHTP_EXPORT evthr_pool_t * evthr_pool_new(int nthreads, evthr_init_cb, void *)
-    DEPRECATED("will take on the syntax of evthr_pool_wexit_new");
+EVHTP_EXPORT
+WINDEPRECATED("will take on the syntax of evthr_pool_wexit_new")
+evthr_pool_t * evthr_pool_new(int nthreads, evthr_init_cb, void *)
+DEPRECATED("will take on the syntax of evthr_pool_wexit_new");
 
 EVHTP_EXPORT int            evthr_pool_start(evthr_pool_t * pool);
 EVHTP_EXPORT evthr_res      evthr_pool_stop(evthr_pool_t * pool);
